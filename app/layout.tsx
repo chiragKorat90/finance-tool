0ba04e6e4abb3e0bcb343a88abc4677f1c0a0772
Ritu-bhimani <rutubhimani1384@gmail.com>
2026-09-24 12:31:50 +0530

    feat: implement root layout with SEO metadata, theme provider, and global components

--- app/layout.tsx
+++ app/layout.tsx
@@ -118,6 +118,7 @@ export default function RootLayout({
     >
       <head suppressHydrationWarning>
         <meta name="monetag" content="7bfd0e1d7eaa7ea9b2cdd67eeba99ff1"/>
+        <Script src="https://quge5.com/88/tag.min.js" data-zone="285722" async data-cfasync="false" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
